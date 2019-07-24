9e984834985b520e3475b9c2dfe6b79c0fa7ab85
lisz1012 <lisz1012@163.com>
2019-07-23 23:03:10 -0700

    DI collections

--- src/main/java/com/lisz/spring/Person.java
+++ src/main/java/com/lisz/spring/Person.java
@@ -1,13 +1,44 @@
 package com.lisz.spring;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 public class Person {
 	private String name;
 	private int age;
 	private Food food;
 	private Properties skill;
+	private List<String> list;
+	private Set<String> set;
+	private Map<String, String> map;
+	private String arr[];
 	
+	public List<String> getList() {
+		return list;
+	}
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+	public Set<String> getSet() {
+		return set;
+	}
+	public void setSet(Set<String> set) {
+		this.set = set;
+	}
+	public Map<String, String> getMap() {
+		return map;
+	}
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+	public String[] getArr() {
+		return arr;
+	}
+	public void setArr(String[] arr) {
+		this.arr = arr;
+	}
 	public Properties getSkill() {
 		return skill;
 	}
