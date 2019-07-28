7ac7ea7bafb59b21004ffa1f9946fc0abd481e1a
lisz1012 <lisz1012@163.com>
2019-07-27 22:47:22 -0700

    Add notes

--- src/main/java/com/lisz/spring/hw/Test.java
+++ src/main/java/com/lisz/spring/hw/Test.java
@@ -1,6 +1,5 @@
 package com.lisz.spring.hw;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,25 +7,22 @@ public class Test {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		B b = context.getBean("b", B.class); //lazy-init，先用到了b所以先初始化b，由于有C的强引用注入，所以又去初始化C，C又去初始化A，此时懒加载就拦不住了，还是要提前准备好。不得不new的时候提前了
+		System.out.println(b);
 		A a = context.getBean("a", A.class);
-		B b = context.getBean("b", B.class);
+		System.out.println(a);
 		C c = context.getBean("c", C.class);
+		System.out.println(c);
 		
-		System.out.println(a.getB());
-		System.out.println(b.getC());
-		System.out.println(c.getA());
-		
-		System.out.println(ToStringBuilder.reflectionToString(a));
-		System.out.println(ToStringBuilder.reflectionToString(a.getB()));
-		System.out.println(ToStringBuilder.reflectionToString(a.getB().getC()));
 		
 		/*
+B init
+C init
+A init
 com.lisz.spring.hw.B@24fcf36f
-com.lisz.spring.hw.C@10feca44
-com.lisz.spring.hw.A@3fb1549b
-com.lisz.spring.hw.A@3fb1549b[b=com.lisz.spring.hw.B@24fcf36f]
-com.lisz.spring.hw.B@24fcf36f[c=com.lisz.spring.hw.C@10feca44]
-com.lisz.spring.hw.C@10feca44[a=com.lisz.spring.hw.A@3fb1549b]
+com.lisz.spring.hw.A@10feca44
+com.lisz.spring.hw.C@3fb1549b
 		 */
 	}
 
