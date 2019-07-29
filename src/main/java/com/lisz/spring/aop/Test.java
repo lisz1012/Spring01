29ff8bf0fdc63a73f1fc62c293e46139b2c00b7d
lisz1012 <lisz1012@163.com>
2019-07-28 20:45:21 -0700

    JDK Dynamic Proxy

--- src/main/java/com/lisz/spring/aop/Test.java
+++ src/main/java/com/lisz/spring/aop/Test.java
@@ -1,9 +1,22 @@
 package com.lisz.spring.aop;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 public class Test {
 
 	public static void main(String[] args) {
-		Human human = new GirlProxy(new Girl());//代理类作装饰
+		final Human girl = new Girl();
+		
+		Human human = (Human)Proxy.newProxyInstance(Girl.class.getClassLoader(), Girl.class.getInterfaces(), new InvocationHandler() {
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				System.out.println("Before");
+				Object retVal = method.invoke(girl, args);
+				System.out.println("After");
+				return retVal;
+			}
+		});
 		human.eat();
 	}
 
