54fc16eb89dd73e9a1f6464f82a57928717d2f89
lisz1012 <lisz1012@163.com>
2019-07-28 17:25:21 -0700

    Autowire annotation, ERROR! 2 objects with the same type autowired

--- src/main/java/com/lisz/spring/dao/MongoDBUserDaoImpl.java
+++ src/main/java/com/lisz/spring/dao/MongoDBUserDaoImpl.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.lisz.spring.entity.User;
 
-//@Repository 暂时不加入， git提交之后展示两个同类型对象的错误//注解，按类型装配, 也不需要写set方法
+@Repository //注解，按类型装配, 也不需要写set方法
 public class MongoDBUserDaoImpl implements UserDao {
 
 	public User getUserByUsername(String username) {
